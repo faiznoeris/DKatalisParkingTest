5a93ac18c1cbff04747dca6c0b744e7365052615
faiznoeris <m.faiznoeris@gmail.com>
2021-05-29 15:59:10 +0700

    updated stdout to output to be able read the result from parking_lot

--- functional_spec/spec/end_to_end_spec.rb
+++ functional_spec/spec/end_to_end_spec.rb
@@ -46,7 +46,7 @@ EOTXT
     it "input from file" do
       command = run("parking_lot #{File.join(File.dirname(__FILE__), '..', 'fixtures', 'file_input.txt')}")
       stop_all_commands
-      expect(command.stdout).to eq(expected)
+      expect(command.output).to eq(expected)
     end
 
   end
